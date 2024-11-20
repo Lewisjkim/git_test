399ca11f899ae6cee5d7ea3bffe626d860a32dd9
JuSeong Kim (Lewis Kim) <93592201+Lewisjkim@users.noreply.github.com>
2024-11-20 14:56:59 +0900

    feat : calculate added calculate functions = sum / mul #2

---
+++ calculate.hpp
@@ -0,0 +1,28 @@
+namespace CALCULATE
+
+{
+    int sum(int a, int b)
+    {
+        return a + b;
+    }
+    float sum(float a, float b)
+    {
+        return a + b;
+    }
+    double sum(double a, double b)
+    {
+        return a + b;
+    }
+    int mul(int a, int b)
+    {
+        return a * b;
+    }
+    float mul(float a, float b)
+    {
+        return a * b;
+    }
+    double mul(double a, double b)
+    {
+        return a * b;
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+#include "calculate.hpp"
+int main()
+{
+    CALCULATE::sum(268,299);
+    CALCULATE::mul(2,544);
+    return 0;
+}
